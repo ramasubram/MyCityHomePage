535143fdf31962d996d0629f757c5fe38b9e6dc3
ramasubram <muraliv_ramas@yahoo.com>
2020-09-16 07:08:25 +0200

    Style changes and extra components

---
+++ app.js
@@ -0,0 +1,8 @@
+//  Javascript for search
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let inputSearchValue = (document.querySelector('.inputSearchValue').value);
+});
